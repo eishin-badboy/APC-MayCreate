47ff090e72150ceb05680c08659cf09ccb4db4a3
Eishin_Badboy <eishin_badboy@yahoo.co.jp>
2020-05-11 17:19:51 +0900

    未解決の外部参照について プログラム書き始めのため、プロジェクトを再度作り直しを検討 dxlibへの設定が間違っている可能性も

--- daikaisen/main.cpp
+++ daikaisen/main.cpp
@@ -4,20 +4,23 @@
 #include <windows.h>
 #include <cmath>
 
-#include "DxLib.h"
+#include <DxLib.h>
 #include "math.h"
 #include "VECTOR2.h"
 
-bool SysInit(void);
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	DxLib_Init();
-
-	// ｼｽﾃﾑ系初期化
-	if (SysInit() == false)
+	// ---------- ｼｽﾃﾑ処理
+	SetGraphMode(800, 600, 16);  	 // 640 ×　480ﾄﾞｯﾄ65536色ﾓｰﾄﾞに設定
+	ChangeWindowMode(true);  // ｳｨﾝﾄﾞｳ ﾓｰﾄﾞで起動
+		// DXﾗｲﾌﾞﾗﾘの初期化
+	SetDrawScreen(DX_SCREEN_BACK);
+	SetWindowText("大海戦");
+	if (DxLib_Init() == -1)
 	{
-		return 0;
+		return -1;  // PG塊（関数）から脱退する
 	}
 
 	// ｹﾞｰﾑﾙｰﾌﾟ
@@ -25,23 +28,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	{
 		DrawBox(300,300,350,350,GetColor(255,255,255),true);
 	}
+
+	return 0;
 }
-
-bool SysInit(void)
-{
-
-	// ---------- ｼｽﾃﾑ処理
-	SetGraphMode(800, 600, 16);  	 // 640 ×　480ﾄﾞｯﾄ65536色ﾓｰﾄﾞに設定
-	ChangeWindowMode(true);  // ｳｨﾝﾄﾞｳ ﾓｰﾄﾞで起動
-	SetWindowText("大海戦");  // ｳｨﾝﾄﾞｳの題名を変更
-
-	if (DxLib_Init() == -1)
-	{
-		return -1;  // PG塊（関数）から脱退する
-	}
-	// DXﾗｲﾌﾞﾗﾘの初期化
-	SetDrawScreen(DX_SCREEN_BACK);
-
-	return true;
-}
-
